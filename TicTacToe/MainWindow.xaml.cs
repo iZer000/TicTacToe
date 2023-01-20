b911496371b6582dcd635704e56d20aac568855c
Zero <77457719+iZer000@users.noreply.github.com>
2023-01-20 21:20:28 +0100

    UI

---
+++ TicTacToe/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace TicTacToe
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GameStart(object sender, RoutedEventArgs e) {}
+
+        private void PlayerClickEvent(object sender, RoutedEventArgs e) {}
+
+    }
+}
---
+++ TicTacToe/Utils.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Media;
+
+namespace TicTacToe
+{
+    public class Utils {
+        private static BrushConverter _converter = new BrushConverter();
+        public static Brush ColorBlue = (Brush)_converter.ConvertFromString("#FFFFFF");
+        public static Brush ColorOrange = (Brush)_converter.ConvertFromString("#FFFFFF");
+        
+        
+    }
+}
